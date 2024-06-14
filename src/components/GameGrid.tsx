b78665c3c322228237262c05e10c6920e21eead1
Wajih Rizvi <swh2rizv@edu.uwaterloo.ca>
2024-06-14 19:47:51 -0400

    Revert "Added infinite scroll"
    This reverts commit 2a3682a86f40be2599d0af901123682b67faa8c5.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,34 +1,26 @@
-import { SimpleGrid, Text } from "@chakra-ui/react"
+import { SimpleGrid, Text, Button } from "@chakra-ui/react"
 import useGames from "../hooks/useGames"
 import GameCard from "./GameCard"
 import CardSkeleton from "./CardSkeleton"
 import GameCardContainer from "./GameCardContainer"
 import { GameQuery } from "../App"
-import InfiniteScroll from "react-infinite-scroll-component"
-import LoadingGames from "./LoadingGames"
+
 interface Props {
     gameQuery: GameQuery
 }
 
 function GameGrid({gameQuery}: Props) {
-    const {data, error, isLoading, fetchNextPage, hasNextPage} = useGames(gameQuery)
+    const {data, error, isLoading, fetchNextPage, isFetchingNextPage} = useGames(gameQuery)
+    console.log(data?.pageParams)
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const totalNumberOfItems = data?.pages.reduce(
-        (accumalator, currentValue) => accumalator + currentValue.length, 
-        0) || 0
     return (
         <>
-            <InfiniteScroll 
-                dataLength={totalNumberOfItems} 
-                next={() => fetchNextPage()} 
-                hasMore={!!hasNextPage}
-                loader={<LoadingGames/>}>
-                <SimpleGrid columns={{sm: 1, md: 3, lg: 4, xl: 4}} spacing='10px'>
-                        {error && <Text>{error.message}</Text>}
-                        {isLoading && skeletons.map((skeleton) => <GameCardContainer key={skeleton} card={<CardSkeleton key={skeleton}/>}/>)}
-                        {data?.pages.map(games => games.map(game => <GameCardContainer key={game.name} card={<GameCard key={game.id} game={game}/>}/>))}
-                </SimpleGrid>
-            </InfiniteScroll>
+            <SimpleGrid columns={{sm: 1, md: 3, lg: 4, xl: 4}} spacing='10px'>
+                {error && <Text>{error.message}</Text>}
+                {isLoading && skeletons.map((skeleton) => <GameCardContainer key={skeleton} card={<CardSkeleton key={skeleton}/>}/>)}
+                {data?.pages.map(games => games.map(game => <GameCardContainer key={game.name} card={<GameCard key={game.id} game={game}/>}/>))}
+            </SimpleGrid>
+            <Button marginTop={3} backgroundColor='green.300' onClick={() => fetchNextPage()}>{isFetchingNextPage ? "Loading..." : "Load More" }</Button>
         </>
 
     )
